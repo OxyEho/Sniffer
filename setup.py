7b815fae0d949ff009794aecab7aba7f96448e2d
OxyEho <valera808@inbox.ru>
2020-12-08 15:58:38 +0500

    change setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="traceroute",
+    name="sniffer",
     version="0.1",
     author="Vedernikov Valera",
     author_email="valera808@inbox.ru",
-    description="package for tracing",
+    description="simple sniffer",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/OxyEho/Traceroute.git",
+    url="https://github.com/OxyEho/Sniffer.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
